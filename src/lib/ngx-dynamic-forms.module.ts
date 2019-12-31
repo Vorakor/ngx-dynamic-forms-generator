ab6e5989a39788f0124566671c7850756955eb66
Shaine Berube <shaine@vorakortech.io>
2019-12-31 14:03:45 -0700

    Updating imports in module

--- src/lib/ngx-dynamic-forms.module.ts
+++ src/lib/ngx-dynamic-forms.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { NgxDynamicFormInputComponent } from "./ngx-dynamic-form-input/ngx-dynamic-form-input.component";
-import { NgxDynamicFormsComponent } from "./ngx-dynamic-forms/ngx-dynamic-forms.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NgxDynamicFormInputComponent } from './ngx-dynamic-form-input/ngx-dynamic-form-input.component';
+import { NgxDynamicFormsComponent } from './ngx-dynamic-forms/ngx-dynamic-forms.component';
 
 @NgModule({
-  declarations: [NgxDynamicFormsComponent],
-  imports: [],
-  exports: [NgxDynamicFormsComponent]
+	declarations: [NgxDynamicFormsComponent, NgxDynamicFormInputComponent],
+	imports: [CommonModule, ReactiveFormsModule, FormsModule],
+	exports: [NgxDynamicFormsComponent],
 })
 export class NgxDynamicFormsModule {}
