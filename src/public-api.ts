8b3b34f2c82387bdeb144aff57bc0791e8c2d492
Shaine Berube <ShaineB@natr-temps.com>
2020-01-03 11:29:34 -0700

    Getting build to work

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of npx-dynamic-forms
  */
 
-export * from "./lib/ngx-dynamic-forms.service";
-export * from "./lib/ngx-dynamic-forms/ngx-dynamic-forms.component";
-export * from "./lib/ngx-dynamic-forms.module";
+export * from './lib/ngx-dynamic-forms/ngx-dynamic-forms.component';
+export * from './lib/ngx-dynamic-forms.module';
